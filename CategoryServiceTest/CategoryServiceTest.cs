480178b8ce6e8823effc121cdb2b03b0f8718e33
aruneash <aruneash123@gmail.com>
2020-07-01 21:13:53 +0530

    Updated

--- CategoryServiceTest/CategoryServiceTest.cs
+++ CategoryServiceTest/CategoryServiceTest.cs
@@ -1,37 +1,66 @@
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OSPCDataAccessLayer.Context;
 using OSPCDataAccessLayer.Entities;
 using OSPCWebApi.Services;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CategoryServiceTest
 {
     [TestClass]
     public class CategoryServiceTest
     {
+        //[TestMethod]
+        //public void TestAddCategory()
+        //{
+        //    // Arrange
+        //    CategoryService service = new CategoryService();
+
+        //    var modelProduct = new List<Product>();
+
+        //    var model = new Category
+        //    {
+        //        Name = "Puma",
+        //        CreatedDate = DateTime.Now,
+        //        IsActive = true,
+        //        Products = modelProduct
+        //    };
+
+        //    // Act
+        //    var actual = service.AddCategory(model);
+
+        //    // Assert
+        //    Assert.AreEqual(1, actual.Result);
+
+        //}
+
         [TestMethod]
-        public void TestAddCategory()
+        public void TestViewCategories()
         {
             // Arrange
             CategoryService service = new CategoryService();
 
-            var modelProduct = new List<Product>();
-
-            var model = new Category
-            {
-                Name = "Puma",
-                CreatedDate = DateTime.Now,
-                IsActive = true,
-                Products = modelProduct
-            };
-
             // Act
-            var actual = service.AddCategory(model);
+            var categories = service.GetViewCategories().Result;
 
             // Assert
-            Assert.AreEqual(1, actual.Result);
+            // Assert.AreNotEqual(expected,actual);
+            Assert.IsTrue(categories.Count == 4);
+            Assert.IsTrue(categories.All(x=>x.Products.Count != 0 ));
+
+        }
+        [TestMethod]
+        public void TestViewCategory()
+        {
+            // Arrange
+            CategoryService service = new CategoryService();
+            int id = 1;
+            // Act
+            var category = service.getViewCategory(id).Result;
+
+            // Assert
+            // Assert.AreNotEqual(expected,actual);
+            Assert.IsTrue(category != null);
+            Assert.IsTrue(category.Products.Count != 0);
 
         }
     }
